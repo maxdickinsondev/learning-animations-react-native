8bc90a5c1501aecfb74274ad6512614c55cbfd98
Max Dickinson <maxsantistadickinson@gmail.com>
2020-06-20 14:56:34 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Animated, Button } from 'react-native';
 
 export default function App() {
+  const [fade] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(fade, {
+      toValue: 1,
+      duration: 5000
+    }).start();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Animated.View 
+        style={{ opacity: fade }}
+      >
+        <View style={styles.fadeIn}>
+          <Text style={styles.text}>Studying animations!</Text>
+        </View>
+    </Animated.View>
     </View>
   );
 }
@@ -16,4 +31,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  fadeIn: {
+    width: 250,
+    height: 50,
+    backgroundColor: 'powderblue'
+  },
+
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 10
+  }
 });
