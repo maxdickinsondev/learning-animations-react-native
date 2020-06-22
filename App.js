465e1ac46a6cce5d3649b45eebbc3a1cedcf0e8b
Max Dickinson <maxsantistadickinson@gmail.com>
2020-06-22 10:04:18 -0300

    Detecting user gestures.

--- App.js
+++ App.js
@@ -1,97 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, StatusBar, View, ImageBackground,
-  Animated, TouchableOpacity, Text
-} from 'react-native';
-
-import bg from './src/assets/background.jpg';
+import React, { useState, useMemo } from 'react';
+import { StyleSheet, View, Animated, PanResponder } from 'react-native';
 
 export default function App() {
-  const [fade] = useState(new Animated.Value(0));
+  const [ball] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
 
-  function signIn() {
-    Animated.spring(fade, {
-      toValue: 370,
-      velocity: 10  
-    }).start();
-  }
+  const panResponder = useMemo(() => PanResponder.create({
+    onMoveShouldSetPanResponder: (e, gestureState) => true,
 
-  function signInFacebook() {
-    Animated.timing(fade, {
-      toValue: 0,
-      duration: 3000
-    }).start();
-  }
+    onPanResponderGrant: (e, gestureState) => {
+      ball.setOffset({
+        x: ball.x._value,
+        y: ball.y._value
+      });
+
+      ball.setValue({ x: 0, y: 0 });
+    },
+
+    onPanResponderMove: Animated.event([null, {
+      dx: ball.x,
+      dy: ball.y
+    }], { listener: (e, gestureState) => {
+      console.log(gestureState);
+    }}),
+
+    onPanResponderRelease: () => {
+      ball.flattenOffset();
+    }
+  }), []);
 
   return (
-    <ImageBackground source={bg} style={styles.image} resizeMode="cover">
-      <View style={{ flex: 1 }}>
-        <StatusBar barStyle="light-content" />
-
-        <View style={styles.areaButton}>
-          <TouchableOpacity style={[styles.buttonSign]} onPress={signIn}>
-            <Text style={styles.textButton}>SIGN IN</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.buttonFacebook} onPress={signInFacebook}>
-            <Text style={[styles.textButton, { color: '#fff' }]}>SIGN IN WITH FACEBOOK</Text>
-          </TouchableOpacity>
-        </View>
-
-        <Animated.View style={[styles.areaAnimated, { top: fade }]}>
-
-        </Animated.View>
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <Animated.View 
+        {...panResponder.panHandlers}
+        style={[
+          styles.ball,
+          {
+            transform: [
+              { translateX: ball.x },
+              { translateY: ball.y } 
+            ]
+          }
+        ]}
+      />
+      
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#fff'
   },
 
-  image: {
-    flex: 1
-  },
-
-  areaAnimated: {
-    height: 200,
-    backgroundColor: '#fff',
-  },
-
-  areaButton: {
-    position: 'absolute',
-    alignItems: 'center',
-    bottom: 0,
-    right: 20,
-    paddingBottom: 40,
-    zIndex: 1
-  },
-
-  buttonSign: {
-    width: 300,
-    height: 50,
-    backgroundColor: '#CCC',
-    borderRadius: 25,
-    margin: 10,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  buttonFacebook: {
-    width: 300,
-    height: 50,
-    backgroundColor: '#4267B2',
-    borderRadius: 25,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
-  textButton: {
-    fontSize: 18,
-    textAlign: 'center'
-  },
+  ball: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#7159c1'
+  }
 });
